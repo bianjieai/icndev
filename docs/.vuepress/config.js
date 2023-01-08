bb9df8655f970a6fd83590aaf2dcf8a61c7659a4
zjb <jinbiao@bianjie.ai>
2023-01-08 10:10:51 +0800

    add og

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,9 +5,9 @@ module.exports = {
     head:[
         ['link', { rel: 'icon', href: '/favicon.ico' }],
         ['meta', { name: 'keywords', content: 'GoN、Game of NFT、Interchain NFTs、IBC、Metadata、ICS-721、Cosmos、IRISnet、NFT Hub' }],
-        ['meta', { name:'og:title',content: 'Game of NFTs'}],
-        ['meta', { name:'og:description',content: 'Game of NFTs (GoN) is an event coming to Cosmos with public incentivized testnets and a hackathon where you can test and build on top of Interchain NFTs.'}],
-        ['meta', { name:'og:image',content: ''}],
+        ['meta', { property:'og:title',content: 'Game of NFTs'}],
+        ['meta', { property:'og:description',content: 'Game of NFTs (GoN) is an event coming to Cosmos with public incentivized testnets and a hackathon where you can test and build on top of Interchain NFTs.'}],
+        ['meta', { property:'og:image',content: ''}],
 
     ],
     themeConfig:{
